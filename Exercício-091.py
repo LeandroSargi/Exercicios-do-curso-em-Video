dc83634c83536e35e21930e0dac07f6f4a1e6381
LeandroSargi <leandro.sargi@hotmail.com>
2019-08-21 22:51:38 -0300

    Revisão final 091

--- Exercício-091.py
+++ Exercício-091.py
@@ -2,13 +2,31 @@
 # aleatórios. Guarde esses resultados em um dicionário, no final, coloque
 # este dicionário em ordem, sabendo que o vencedor tirou o maior número no
 # dado.
+from time import sleep
 from random import randint
+from operator import itemgetter #módulo para colocar dicionário em ordem
 jogada = dict()
-
+ranking = list()
 
 jogada['Jogador1'] = randint(1,6)
 jogada['Jogador2'] = randint(1,6)
 jogada['Jogador3'] = randint(1,6)
 jogada['Jogador4'] = randint(1,6)
 
-print(jogada)
+print('-='*20)
+print('{:^40}'.format('Sorteando'))
+print('-='*20)
+
+for k, v in jogada.items():
+    print(f'O {k} tirou {v}')
+    sleep(1)
+
+print('-='*20)
+print('Ranking dos jogadores:')
+
+ranking = sorted(jogada.items(), key=itemgetter(1), reverse=True)
+#Sorteando items da jogada, levando em consideração a chave 1 que são
+# os valores dos dados, modo reverso verdadeiro
+
+for i, v in enumerate(ranking):
+    print(f'{i+1}° lugar >>> {v[0]} com {v[1]} pontos.')
