58fe72bcc38d8b9a91b03aa5aaec648a1407438f
LeandroSargi <leandro.sargi@hotmail.com>
2019-09-05 17:23:38 -0300

    Exercício 102 adicionado completamente

--- Exercício-102.py
+++ Exercício-102.py
@@ -3,19 +3,28 @@
 #chamado show, que será um valor lógico (opcional) indicando se será
 #mostrado ou não na tela o processo de cálculo do fatorial.
 
-def fatorial(num):
+def fatorial(num, show=False):
+    """
+    --> Calcula o fatorial de um número.
+    :param num: recebe o valor para cálculo do fatorial.
+    :param show: mostra ou não as etapas do cálculo (padrão=False).
+    :return: retorna o resultado do fatorial do parâmetro num.
+    """
     fat = 1
     c = num
     while c > 0:
+        if show:
+            print(f'{c} ', end='')
+            if c > 1:
+                print(f'{"x"} ', end='')
+            else:
+                print(f'{"="} ', end='')
         fat *= c
         c -= 1
     return(fat)
 
-# def show():
-#     if show True:
 
-
-
+#Programa Principal
 num = int(input('Digite um número para saber o seu fatorial:'))
-
-print(fatorial(num))
+print(fatorial(num, show=True))
+#help(fatorial)
